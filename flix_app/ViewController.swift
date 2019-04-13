bf3202cd758b6dd3af9e7b90c3aa2e0936e1efec
Malcolm Treacy <mtreacy@dhcp-v093-124.mobile.uci.edu>
2019-04-13 11:02:39 -0700

    initial project upload

---
+++ flix_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix_app
+//
+//  Created by Malcolm Treacy on 4/13/19.
+//  Copyright © 2019 malctech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
